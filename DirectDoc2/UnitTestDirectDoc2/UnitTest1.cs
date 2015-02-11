458cf6a63039a382fffb4b159da470fdc59b2199
AdewaleOdunlami <adewaleodunlami@yahoo.com>
2015-02-11 05:28:23 -0500

    Database updated

---
+++ DirectDoc2/UnitTestDirectDoc2/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DirectDoc2.Controllers;
+//using System.Web.Mvc;
+
+namespace UnitTestDirectDoc2
+{
+    [TestClass]
+    public class PersonControllerTest
+    {
+        [TestMethod]
+        public void TestDeleteConfirmed()
+        {
+            //var controller = new PersonController();
+            //var result = controller.DeleteConfirmed(5);
+            //Assert.AreEqual("Index", "action");
+            
+        }
+    }
+}
